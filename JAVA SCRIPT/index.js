761b20c46f47e0bef0b3ca2648ece9c65f35ef1a
MubasheerHussain11 <137383951+MubasheerHussain11@users.noreply.github.com>
2023-11-15 03:40:47 +0530

    javacript added

---
+++ JAVA SCRIPT/index.js
@@ -0,0 +1,8 @@
+function removewindow(){
+    document.getElementById("logcontainer").style.display="none"
+}
+
+function scroll()
+{
+    document.getElementById("secondpage").style.scrollMargin="100vh"
+}
